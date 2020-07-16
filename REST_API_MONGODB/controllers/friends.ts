c2eed557305d73bb08fb4d8f23e55cb1884dbc00
Nitin Sabale <nitinsabale59@gmail.com>
2020-07-16 16:35:28 +0530

    Removed Unusable code

--- REST_API_MONGODB/controllers/friends.ts
+++ REST_API_MONGODB/controllers/friends.ts
@@ -20,71 +20,19 @@ const getAllFriends = async (ctx: RouterContext) => {
 };
 
 const addFriend = async (ctx: RouterContext) => {
-  const friends = await friendsCollections.find();
-  if (friends) {
-    ctx.response.status = 200;
-    ctx.response.body = {
-      success: false,
-      data: friends,
-    };
-  } else {
-    ctx.response.status = 400;
-    ctx.response.body = {
-      success: false,
-      msg: "You dont have ay friends",
-    };
-  }
+  
 };
 
 const getFriendById = async (ctx: RouterContext) => {
-  const friends = await friendsCollections.find();
-  if (friends) {
-    ctx.response.status = 200;
-    ctx.response.body = {
-      success: false,
-      data: friends,
-    };
-  } else {
-    ctx.response.status = 400;
-    ctx.response.body = {
-      success: false,
-      msg: "You dont have ay friends",
-    };
-  }
+  
 };
 
 const updateFriend = async (ctx: RouterContext) => {
-  const friends = await friendsCollections.find();
-  if (friends) {
-    ctx.response.status = 200;
-    ctx.response.body = {
-      success: false,
-      data: friends,
-    };
-  } else {
-    ctx.response.status = 400;
-    ctx.response.body = {
-      success: false,
-      msg: "You dont have ay friends",
-    };
-  }
+  
 };
 
 const deleteFriend = async (ctx: RouterContext) => {
-  const friends = await friendsCollections.find();
-  if (friends) {
-    ctx.response.status = 200;
-    ctx.response.body = {
-      success: false,
-      data: friends,
-    };
-  } else {
-    ctx.response.status = 400;
-    ctx.response.body = {
-      success: false,
-      msg: "You dont have ay friends",
-    };
-  }
+  
 };
 
 export { getAllFriends, addFriend, getFriendById, updateFriend, deleteFriend };
